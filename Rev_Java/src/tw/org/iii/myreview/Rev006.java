8b7e8d3c4e26830531e69d79fcf08082df4adef0
Seanhhe <seanhhe@gmail.com>
2018-12-04 03:53:13 +0800

    2018-12-04_Review_Java

---
+++ Rev_Java/src/tw/org/iii/myreview/Rev006.java
@@ -0,0 +1,19 @@
+package tw.org.iii.myreview;
+
+public class Rev006 {
+
+	public static void main(String[] args) {
+		// boolean
+		// Java 無0與1的判斷, 只有true or false
+		boolean b1 = true;
+		boolean b2 = false;
+		
+		int a = 10, b = 3;
+		boolean b3 = a == b;
+		System.out.println(b3);
+		
+		boolean b4 = a > b;
+		System.out.println(b4);
+	}
+
+}
