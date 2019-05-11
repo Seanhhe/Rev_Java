599376f1ec4828e6e96f0d51411880d9dc5b82e4
SEAN <SEAN@DESKTOP-1BIR0RB>
2019-05-11 20:19:09 +0800

    2019-05-10_Review_Java

--- Rev_Java/src/tw/org/iii/myreview/Rev042.java
+++ Rev_Java/src/tw/org/iii/myreview/Rev042.java
@@ -4,13 +4,17 @@ package tw.org.iii.myreview;
  * 
  */
 
+
 public class Rev042 {
 
 	public static void main(String[] args) {
 		Rev001 obj1 = new Rev001();
 		
 		// 名字相同但import只能寫一個，故須把完整檔名寫出 p2.Rev001
+//		p2.Rev001 obj2 = new p2.Rev001();
 		
+		// 因為存取修飾字： p2.Lab02 非public 只能由相同package的類別來存取
+		// p2.Lab02 obj2 = new p2.Lab02();
 	}
 
 }
