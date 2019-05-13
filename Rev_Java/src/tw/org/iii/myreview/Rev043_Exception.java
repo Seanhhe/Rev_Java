52012f1a7acd461d49721a8e7523581edb793e80
SEAN <SEAN@DESKTOP-1BIR0RB>
2019-05-14 01:10:55 +0800

    2019-05-13_Review_Java

--- Rev_Java/src/tw/org/iii/myreview/Rev043_Exception.java
+++ Rev_Java/src/tw/org/iii/myreview/Rev043_Exception.java
@@ -28,7 +28,65 @@ public class Rev043_Exception {
 		int[] d = new int[3];
 		//System.out.println("d[3]: " + d[3]); //超出範圍，index共三個只有0,1,2。
 		
+//		int a = 10, b = 3;
+//		int c = a / b;
+//		System.out.println(c); // 成功執行：3
+//		//System.out.println(d[3]); // 拋出例外
+//		System.out.println("GG"); // 上一行程式提前終止，所以此行沒有執行
 		
+//		try {
+//			System.out.println("d[3]: " + d[3]);
+//		} catch (ArrayIndexOutOfBoundsException e) {
+//			// 捕捉到例外後的處理方法 (沒寫就是不處理)
+//			// 處理完後，程式繼續執行
+//			System.out.println("有捕捉到例外喔");
+//		}
+//		System.out.println("GG");
+		
+//		try {
+//			System.out.println("d[3]" + d[3]);
+//		} catch (ArithmeticException e) {
+//			// 預設要抓的例外非實際發生的-->抓不到
+//			System.out.println("預設要抓的例外非實際發生的-->抓不到");
+//		}
+//		System.out.println("GG");
+		
+//		try {
+//			System.out.println("Before");
+//			System.out.println(d[13]);
+//			System.out.println("After"); // 這句並沒有執行，因為前一句有例外產生(有狀況)直接跳到catch
+//		} catch (ArrayIndexOutOfBoundsException e) {
+//			System.out.println("有狀況1");
+//		} catch (ArithmeticException e) {
+//			// 增加其他例外條件，讓程式一個一個判斷
+//			System.out.println("有狀況2");
+//		}
+//		System.out.println("END");
+		
+		System.out.println("--------");
+		
+		try {
+			b = 0;
+			int c = a / b;
+			System.out.println(c); // 這裡先有狀況就拋出
+			System.out.println(d[13]); // 前一行有狀況，所以此行根本沒執行，雖然此行也有狀況
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("有狀況1");
+		} catch (ArithmeticException e) {
+			System.out.println("有狀況2");
+		}
+		System.out.println("END");
+		
+		/*		前面練習中的例外關係圖
+		 * 		Exception
+		 * 		|-->RuntimeException
+		 * 				|--> ArithmeticException
+		 * 				|--> I..
+		 * 						|--> Array
+		 * 		catch的順序
+		 * 		-->如果非直系血親=沒差
+		 * 		-->若為直系血親-->子類別(細部處理)要放在父類別上面
+		 */
 	}
 
 }
