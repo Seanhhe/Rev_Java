240f488507ba806b99debf711164a42023c8d333
SEAN <SEAN@DESKTOP-1BIR0RB>
2019-10-09 13:13:35 +0800

    2019-10-07_Review_Java

--- Rev_Java/src/tw/org/iii/myreview/Rev062_Net2.java
+++ Rev_Java/src/tw/org/iii/myreview/Rev062_Net2.java
@@ -1,5 +1,15 @@
 package tw.org.iii.myreview;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 /*		20180818AM1		網路基本概論 00:54:00  
+ * 
+ * 		接收端
+ * 
  * 		Brad60 (DatagramSocket 接收端)
  * 		
  * 		cmd ping --> 可以證明對方在，但不能證明對方不在
@@ -26,13 +36,48 @@ package tw.org.iii.myreview;
  * 
  * 		讀取資料要找接收端的packet(詳細的發送端資料) & buffer(傳送的封包內容)
  * 
- * 		*
+ * 		*如果要持續接收，可跑無窮迴圈？
  * 
  */
 public class Rev062_Net2 {
 
 	public static void main(String[] args) {
 		
+		// 接收用的陣列 
+		byte[] buf = new byte[1024]; // 指定接收用陣列之長度
+		
+		try {
+			// 接收一次性的封包 (建立物件並指定通訊port)
+			DatagramSocket socket = new DatagramSocket(9999);
+			
+			// 接收用封包 (建立物件封包並指定 接收的陣列物件 & 其陣列物件的長度 )
+			DatagramPacket packet = new DatagramPacket(buf, buf.length);
+			
+			System.out.println("Before");
+			// 啟動接收封包
+			socket.receive(packet);
+			
+			System.out.println("After");
+			
+			// 關閉封包
+			socket.close(); // 是不是要關閉封包後，才可以存取封包內容？
+			
+			// 存取接收到的封包內容
+			byte[] data = packet.getData();
+			// 封包資料長度
+			int len = packet.getLength();
+			//
+			int dataLen = data.length;
+			InetAddress ip = packet.getAddress();
+			System.out.println(ip.getHostAddress() + " : " + len + " : " + new String(data) + "封包長度：" + dataLen);
+			
+		} catch (SocketException e) {
+			//	SocketException | IOException 非直系血親可這樣用
+			//	例外發生：該port號被占用			
+			System.out.println("SocketException : " + e);
+		} catch (IOException e) {
+			System.out.println("IOException : " + e);
+		}
 	}
 
 }
