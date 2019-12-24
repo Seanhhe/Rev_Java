35b3be96e1ec3586c285ac2bf0da5a3e00c8bd46
SEAN <SEAN@DESKTOP-1BIR0RB>
2019-12-24 16:07:34 +0800

    2019-12-22_Review_Java

--- Rev_Java/src/tw/org/iii/myreview/Rev089_Rev040_MyPanelV1.java
+++ Rev_Java/src/tw/org/iii/myreview/Rev089_Rev040_MyPanelV1.java
@@ -1,6 +1,8 @@
 package tw.org.iii.myreview;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -8,6 +10,14 @@ import javax.swing.JPanel;
 
 /*	20180811AM1
  * 	追加點選畫面，圖案出現在滑鼠位置，且滑鼠點擊位置是該圖案中心。
+ * 
+ * 	作法一：
+ * 	Rev089_Rev040_MyPanelV1類別繼承了JPanel，並由MouseListener實作
+ * 	
+ * 	作法二：
+ * 	Java 2D繪圖範例架構+鍵盤滑鼠事件
+ * 	http://selfinquiring.hatenablog.com/entry/2017/03/11/220901
+ * 		採用Components而不用JPanel或Canvas 是因為內建有雙緩衝，更新畫面不會閃
  */
 
 public class Rev089_Rev040_MyPanelV1 extends JPanel implements MouseListener { // 把這支副程式整個當作一個群組
@@ -19,7 +29,7 @@ public class Rev089_Rev040_MyPanelV1 extends JPanel implements MouseListener { /
 	// 建構式
 	public Rev089_Rev040_MyPanelV1() {
 		System.out.println("Rev089_Rev040_MyPanelV1 : 無傳參數建構式");
-		//addMouseListener(this);
+		addMouseListener(this);
 	}
 	
 	public Rev089_Rev040_MyPanelV1(int ballx, int bally) {
@@ -32,33 +42,75 @@ public class Rev089_Rev040_MyPanelV1 extends JPanel implements MouseListener { /
 		// this => Rev089_Rev040_MyPanelV1 本人聽
 	}
 
+	/*		
+	 * 
+	 */
+	
+	/*		需要手動右鍵加入override方法
+	 * 		(JComponent)
+	 * 		注意修飾字 => protected
+	 */
+	
+	// 繼承JPanel(父類別JComponent)的方法 => Override
 	@Override
-	public void mouseClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub
+	protected void paintComponent(Graphics g) {
+		//		component的生命週期  (沒有呼叫卻自動執行)
+		//		component有被呼叫，並傳遞Graphics物件
+		//		為何 component 會自動產生?
+		//		傳入的 Graphics 已產生物件實體 (Graphic抽象類別的子類別 Graphic2D 中的 Inner class所產生)
 		
+		/*	該物件具有生命週期  20180811AM1
+		 * 	當生命週期到了，其呼叫的方法非開發者所建立
+		 * 	開發者呼叫某方法導致觸發它，而那些方法才被呼叫
+		 */
+		super.paintComponent(g);
+		System.out.println("Paint");
+		//把g強制轉型回Graphics2D [骨子裡是Graphic2D 不清楚可用instanceof ]
+		Graphics2D g2d = (Graphics2D)g;
+		g2d.setColor(Color.ORANGE); // 程式依序執行，先set，後繪圖，才看得到顏色
+		g2d.drawOval(ballx, bally, 50, 50); // drawOval 畫橢圓 (X正值往右，Y正值往下)
+		g2d.fillOval(ballx, bally, 50, 50);
+		
+	}
+	
+	// MouseListener 並沒有重新implement成另一個類別
+	// 而是直接實作在同一類別內，方便後續修改與存取類別內的屬性
+	
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// 新增mouseListener的實作方法
+		System.out.println("V1 mouseClicked()");
+		ballx = e.getX()-25; // 減去半徑
+		bally = e.getY()-25;
+		repaint();	// 父類別的repaint與子類別repaint執行順序為何？ 之後設計類似的程式要注意
+		// repaint 類似會被push到旁邊等color做完？ 註解repaint()試試看
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
+		// 新增mouseListener的實作方法
 		
 	}
 
 	@Override
 	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
+		// 新增mouseListener的實作方法
 		
 	}
 
 	@Override
 	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
+		// 新增mouseListener的實作方法
 		
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
-		// TODO Auto-generated method stub
+		// 新增mouseListener的實作方法
 		
 	}
+	
 }
+
+// HW: 改成讓滑鼠點到的地方變成是圓心
--- Rev_Java/src/tw/org/iii/myreview/Rev089_Rev040_interface3_window.java
+++ Rev_Java/src/tw/org/iii/myreview/Rev089_Rev040_interface3_window.java
@@ -1,6 +1,11 @@
 package tw.org.iii.myreview;
 
 import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 import javax.swing.JFrame;
 
@@ -22,22 +27,127 @@ public class Rev089_Rev040_interface3_window extends JFrame {
 	// 建構式
 	public Rev089_Rev040_interface3_window() {
 		// 放最前面
-		setLayout(new BorderLayout());
+		setLayout(new BorderLayout()); // 先setLayout才能使用add方法
 		
 		myPanel = new Rev089_Rev040_MyPanelV1(); // 僅產生物件實體
 		add(myPanel, BorderLayout.CENTER);
 		
+		// 加入滑鼠監聽事件
+		addMouseListener(new MyClicker());
 		
 		
-		// 放在最後
+		// 放在最後 (視窗大小&顯示視窗)
 		setSize(640, 480);
 		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setDefaultCloseOperation(EXIT_ON_CLOSE); // 關閉視窗方法一
+		
+		//	關閉視窗方法二
+//		addWindowListener(new MyWindowListener());
+		
+		//	關閉視窗方法三
+		addWindowListener(new MyListener03());
 		
 	}
 	
 	public static void main(String[] args) {
-		
+		// 程式的執行點
+		new Rev089_Rev040_interface3_window();
 	}
 
+}
+
+// 滑鼠事件
+class MyClicker extends MouseAdapter {
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		super.mouseClicked(e);
+		System.out.println(e.getX()); // 透過getX()方法把值傳回e(滑鼠事件)
+		System.out.println(e.getY());
+		new Rev089_Rev040_MyPanelV1(); // 啟動MypanelV1()，如此才能從e取出XY值 並repaint()
+	}
+	
+}
+
+//----------------------------------------------
+/*		不用setDefaultCloseOperation(EXIT_ON_CLOSE)方法關閉視窗
+ * 		改用下面的方法：WindowListener
+ * 		但只需要其中的 closing 方法
+ * 		改用 class WindowAdapter (implements WindowListener的抽象類別)
+ */
+
+// 關閉視窗方法二
+//class MyWindowListener implements WindowListener {
+//	/*	建立MyWindowListener 實現 WindowListener 建立 WML 物件實體
+//	 * 	接收視窗活動訊息
+//	 */
+//	@Override
+//	public void windowActivated(WindowEvent arg0) {
+//		System.out.println("windowActivated(WindowEvent arg0)");
+//	}
+//
+//	@Override
+//	public void windowClosed(WindowEvent arg0) {
+//		System.out.println("windowClosed(WindowEvent arg0)");
+//	}
+//
+//	@Override
+//	public void windowClosing(WindowEvent arg0) {
+//		System.out.println("windowClosing(WindowEvent arg0)");
+//		System.exit(0); // 關閉程式
+//		//	參數0 => command line return value
+//		//	回傳給系統上一道指令完成的return值
+//		//	通常return 0 代表 normal	/	非 0 代表 error
+//	}
+//
+//	@Override
+//	public void windowDeactivated(WindowEvent arg0) {
+//		System.out.println("windowDeactivated(WindowEvent arg0)");
+//	}
+//
+//	@Override
+//	public void windowDeiconified(WindowEvent arg0) {
+//		System.out.println("windowDeiconified(WindowEvent arg0)");
+//	}
+//
+//	@Override
+//	public void windowIconified(WindowEvent arg0) {
+//		System.out.println("windowIconified(WindowEvent arg0)");
+//	}
+//
+//	@Override
+//	public void windowOpened(WindowEvent arg0) {
+//		System.out.println("windowOpened(WindowEvent arg0)");
+//	}
+//	
+//}
+
+//	關閉視窗方法三
+class MyListener03 extends WindowAdapter {
+	//	繼承抽象類別 WindowAdapter 並加以 override 實作
+	//	缺點是不能再找父類別繼承 (不能雙重繼承)
+	
+	@Override
+	public void windowActivated(WindowEvent e) {
+		super.windowActivated(e);
+		System.out.println("windowActivated(WindowEvent e)");
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		super.windowClosing(e);
+		System.out.println("windowClosing(WindowEvent e)");
+		System.exit(0);	//	關閉程式
+		/*	參數 0 => command line return value
+		 * 	回傳給系統上一道指令完成的 return 值
+		 * 	通常 return 0 代表 normal / 非0 代表 error
+		 */
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		super.windowDeactivated(e);
+		System.out.println("windowDeactivated(WindowEvent e)");
+	}
+	
 }
